97779ec80647aaf018060d39fc5a2076f49a1653
Saicharan Reddy <saicharan.reddy1@gmail.com>
2017-12-07 21:28:25 +0530

    Added the JSON file

---
+++ project1/p1.py
@@ -0,0 +1,26 @@
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+def translate(word):
+    word = word.lower()
+    lnth = len(get_close_matches(word, data.keys()))
+    matches=get_close_matches(word,data.keys())
+    if word in data:
+
+        return data[word]
+
+    elif lnth > 0:
+        print("Did you mean %s?"%get_close_matches(word,data.keys())[0],)
+        word = input("Enter yes/no: ")
+        if(word=='yes'):
+            print(translate(word))
+        elif(word=='no'):
+            word = input("Enter word: ")
+            print(translate(word))
+
+    else:
+        return 'Please check the spelling'
+word = input("Enter word: ")
+print(translate(word))
+
+
