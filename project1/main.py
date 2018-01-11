c59b5cc6c1e90f97263adbcdeb3f57f699b0c599
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-01-11 18:30:13 +0530

    Update main.py

--- project1/main.py
+++ project1/main.py
@@ -2,7 +2,7 @@ import json
 import sys
 from difflib import get_close_matches
 from PyDictionary import PyDictionary
-
+import pyttsx
 
 data = json.load(open("data.json"))
 
@@ -13,6 +13,10 @@ def display_initial_message():
     print "//////////////////////////////////////////////////////"
     print "\n\n1. To search a word, just type in at the prompt.\n2. You can choose to view the Synonymns and the Antonymns of the word.\n3. To quit, type quit().\n4. To display this message again, type message().\n"
 
+def textToSpeech(word):
+	engine = pyttsx.init()
+	engine.say(word)
+	engine.runAndWait()
 
 def synonym(word):
     word = word.lower()
@@ -41,11 +45,29 @@ def antonym(word):
 		anto_list = (dictionary.antonym(word))
 		print("The antonym(s) of the word %s are:"%word)
 		for i in range(0,len(anto_list)):
+			antonym += [anto_list[i].encode('ascii')]
 			print (str(i+1)+')'+anto_list[i].encode('ascii'))
+			textToSpeech(antonym[i])
 	except TypeError:
-		word = raw_input("Re-enter the word with the correct spelling: ")
-		antonym(word)
+		print("Please re enter the world below with the right spelling! ")
+
+
+def antonym(word):
+	try:
+		word = word.lower()
+		dictionary = PyDictionary()
+		anto_list = (dictionary.antonym(word))
+		print("The antonym(s) of the word %s are:"%word)
+		textToSpeech("The antonyms of the word you entered are")
+		antonym=[]
+		for i in range(0,len(anto_list)):
+			antonym += [anto_list[i].encode('ascii')]
+			print (str(i+1)+')'+anto_list[i].encode('ascii'))
+			textToSpeech(antonym[i])
 	
+	except TypeError:
+		print("Please re enter the world below with the right spelling! ")
+
 
 if(__name__=='__main__'):
 	display_initial_message()
@@ -61,6 +83,11 @@ if(__name__=='__main__'):
 				antonym = antonym(word)
 			elif(choice=='S' or choice=='s'):
 				synonym = synonym(word)
-				print synonym
+				print("The synonym(s) of the word %s are:"%word)
+				textToSpeech("The synonyms of the word that you entered are")
+				for i in range (0,len(synonym)):
+					synonym[i]=synonym[i].encode('ascii')
+					print (str(i+1)+')'+synonym[i])
+					textToSpeech(synonym[i])
 			elif(choice=='Q' or choice=='q'):
 				sys.exit()
